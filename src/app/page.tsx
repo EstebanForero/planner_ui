1a0331debb07bd1b5923fde9b969944dead0e5da
Esteban Forero <estebanmff@outlook.com>
2024-12-04 10:18:38 -0500

    tanstack added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Calendar from "./sections/Calendar";
 import Classes from "./sections/Classes";
 import NavBar from "./sections/NavBar";
 
+const queryClient = new QueryClient()
+
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <NavBar/>
-      <div className="w-full h-full px-8 py-6">
-        <Calendar/>
-        <Classes/>
+    <QueryClientProvider client={queryClient}>
+      <div className="w-full h-full">
+        <NavBar/>
+        <div className="w-full h-full px-8 py-6">
+          <Calendar/>
+          <Classes/>
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
