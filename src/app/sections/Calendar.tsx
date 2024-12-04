07bfd494df05049fd89af0a3f983b731cf8fafce
RustMaster-arch <rustmasterfullstack@gmail.com>
2024-12-04 12:47:51 -0500

    added functionality buttons and future use full features

--- src/app/sections/Calendar.tsx
+++ src/app/sections/Calendar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { Element } from 'react-scroll';
 import { useQuery } from '@tanstack/react-query';
 import { get_planning as obtain_planning, WeeklySchedule } from '@/lib/planner_backend';
@@ -14,6 +14,8 @@ interface Props {
 }
 
 const Calendar = ({ userId }: Props) => {
+  const [count, setCount] = useState(0);
+
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const timeSlots = [
     '7am', '8am', '9am', '10am', '11am', '12pm', 
@@ -43,6 +45,21 @@ const Calendar = ({ userId }: Props) => {
     'Sat': 'Saturday',
   }
 
+  const handler = () => {
+    if (count == data.length - 1) {
+      return
+    }
+    setCount(count + 1);
+    return
+  }
+
+  const prevHandler = () => {
+    if (count == 0) {
+      return
+    }
+    setCount(count - 1);
+  }
+
   return (
     <section className="mb-3 w-full h-full">
       <Element name="calendar" className="w-full h-full">
@@ -65,7 +82,7 @@ const Calendar = ({ userId }: Props) => {
 
           {daysOfWeek.map((day, dayIndex) => {
             const actualDayKey = dayMap[day];
-            const daySchedule = data[0]?.[actualDayKey];
+            const daySchedule = data[count]?.[actualDayKey];
 
             if (!daySchedule) return null
 
@@ -80,7 +97,33 @@ col-span-1 row-span-1 bg-red-500 text-white flex justify-center items-center`}>
               );
             });
           })}
+
         </div>
+
+        <button className='bg-transparent border rounded border-purple-800 text-white w-16 h-10'
+          onClick={handler}>
+          Next
+        </button>
+
+        <button className='bg-transparent border rounded border-purple-800 text-white w-16 h-10 ml-3'
+          onClick={prevHandler}>
+          Prev 
+        </button>
+
+        <input className='ml-4 bg-transparent border border-purple-800 text-white w-30 h-10 p-2
+          placeholder:p-1 placeholder:text-gray-400 placeholder:m-2 placeholder:bg-gray-800
+          placeholder:rounded rounded'
+          placeholder='hour cost'/>
+
+        <input className='ml-4 bg-transparent border border-purple-800 text-white w-30 h-10 p-2
+          placeholder:p-1 placeholder:text-gray-400 placeholder:m-2 placeholder:bg-gray-800
+          placeholder:rounded rounded'
+          placeholder='day cost'/>
+
+        <button className='bg-transparent border rounded border-purple-800 text-white w-30 h-10 ml-3 p-2'
+          onClick={prevHandler}>
+          Ranked plannings
+        </button>
       </Element>
     </section>
   );
